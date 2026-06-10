7b52c015967bff9943f7a4886828d313000b10b3
dominika <dominika.oles@invicara.com>
2026-06-10 14:52:15 +0100

    Upgrade React and Webpack dependency compatibility
    - Added a Webpack NormalModuleReplacementPlugin to rewrite node:* imports, such as node:stream, to standard module names like stream.
    - Upgraded react and react-dom from React 17 to React 18.
    - Upgraded canvas to the latest version so the app can be installed on Node.js versions greater than 18.

--- starter-app-source/webpack.prod.js
+++ starter-app-source/webpack.prod.js
@@ -77,7 +77,10 @@ const CONFIG = {
     new webpack.ProvidePlugin({
       process: 'process/browser.js',
       Buffer: ['buffer', 'Buffer'],
-    })
+    }),
+    new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+      resource.request = resource.request.replace(/^node:/, "");
+    })    
   ],
 
   module: {
